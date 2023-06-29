f137593df1528b0ab799b24c7927fd802f737296
Utkarsh <utkarshsingh6207603306@gmail.com>
2023-06-29 14:57:22 +0530

    this is the initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import AllCards from './components/AllCards';
+import Navbar from './components/Navbar'
+import Typography from './components/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <Typography/>
+        <AllCards/>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/components/Typography.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Typography() {
+  return (
+    <figure className="text-center text-bg-info p-3">
+        <blockquote className="blockquote">
+            <h1 className="display-3 ">Utkarsh Best Sellers</h1>
+        Our most popular products based on sales. Updated frequently.
+        </blockquote>
+    </figure>
+  )
+}
